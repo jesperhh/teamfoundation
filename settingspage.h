dfa2bb2d13553fa1905628a2d2afe6a66faf6196
Jean Gressmann <jean@0x42.de>
2016-05-20 18:18:44 +0200

    Fixes build on Linux

--- settingspage.h
+++ settingspage.h
@@ -2,17 +2,17 @@
 **
 ** Team Foundation Server plugin for Qt Creator
 ** Copyright (C) 2014 Jesper Hellesø Hansen
-** 
+**
 ** This library is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU Lesser General Public
 ** License as published by the Free Software Foundation; either
 ** version 2.1 of the License, or (at your option) any later version.
-** 
+**
 ** This library is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ** Lesser General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU Lesser General Public
 ** License along with this library; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -25,6 +25,8 @@
 #include "ui_settingspage.h"
 
 #include <vcsbase/vcsbaseoptionspage.h>
+
+#include <QPointer>
 
 QT_BEGIN_NAMESPACE
 class QSettings;
